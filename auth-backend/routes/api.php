00d625fbf34d40be352af6b770e172e07c59df55
Coding_by-dhruv <167305177+Dhruv-9311@users.noreply.github.com>
2026-04-30 12:34:51 +0530

    add jwt protected routes for task CRUD endpoints

--- auth-backend/routes/api.php
+++ auth-backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
@@ -10,4 +11,5 @@ Route::prefix('auth')->group(function () {
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
+    Route::apiResource('tasks', TaskController::class);
 });
